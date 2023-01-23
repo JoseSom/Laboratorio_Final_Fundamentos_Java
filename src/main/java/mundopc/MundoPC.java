997ae9efef9197e2777b162ea39ac728275213fb
JoseSom <joseluissom004@gmail.com>
2023-01-23 11:23:03 -0600

    Se agrega ejemplo final

--- src/main/java/mundopc/MundoPC.java
+++ src/main/java/mundopc/MundoPC.java
@@ -1,7 +1,16 @@
 package mundopc;
 
+import com.gm.mundopc.*;
+
 public class MundoPC {
     public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP", 13);
+        Teclado tecladoHP = new Teclado("BT", "HP");
+        Raton ratonHP = new Raton("BT", "HP");
+        Computadora computadoraHP = new Computadora("ComputadoraHP", monitorHP, tecladoHP, ratonHP);
 
+        Orden ordenHP = new Orden();
+        ordenHP.agregarComputador(computadoraHP);
+        ordenHP.mostrarOrden();
     }
 }
